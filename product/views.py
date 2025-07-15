1d48e65141daf2ee0ce314740e35e7db2ec334f4
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-16 00:52:33 +0600

    Customizing Generic Views

--- product/views.py
+++ product/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from product.models import Product, Category
 from product.serializers import ProductSerializer, CategorySerializer
 from django.db.models import Count
@@ -21,25 +21,6 @@ class ViewProducts(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class ProductsList(ListCreateAPIView):
-    queryset = Product.objects.select_related("category").all()
-    serializer_class = ProductSerializer
-
-    """
-    Normal view/something er jonno Normal-vabe use hobe...
-    Logical kaj er jonno method/function nite hobe...
-    """
-
-    # def get_queryset(self):
-    #     return Product.objects.select_related("category").all()
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
-
-    # def get_serializer_context(self):
-    #     return {"request": self.request}
 
 
 class ViewSpecificProduct(APIView):
@@ -76,11 +57,6 @@ class ViewCategory(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class CategoryList(ListCreateAPIView):
-    queryset = Category.objects.annotate(product_count=Count("products")).all()
-    serializer_class = CategorySerializer
-
-
 class ViewSpecificCategory(APIView):
     # ---> Defining same data using method...
     def get_category(self, id):
@@ -104,3 +80,40 @@ class ViewSpecificCategory(APIView):
         category = self.get_category(id)
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# -------> Generic Views in Below...
+
+
+class ProductsList(ListCreateAPIView):
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()  # --> Generic View te normal query kaj kore...
+    serializer_class = ProductSerializer
+
+    lookup_field = "id"
+
+    # # -----------> Logical bepar-separ thakle sadharonto use kori...
+    # def delete(self, request, id):
+    #     product = get_object_or_404(Product, pk=id)
+    #     if product.stock > 10:
+    #         return Response(
+    #             {"message": "Product with stock more than 10 could't be delete !"}
+    #         )
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryList(ListCreateAPIView):
+    queryset = Category.objects.annotate(product_count=Count("products")).all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.annotate(product_count=Count("products")).all()
+    serializer_class = CategorySerializer
+
+    lookup_field = "id"
