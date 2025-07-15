b3a4bc740a17114a0bebd4f67f34b22b296bccca
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-16 01:01:18 +0600

    Ready for Model View Set

--- product/views.py
+++ product/views.py
@@ -5,31 +5,24 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+#
 from product.models import Product, Category
 from product.serializers import ProductSerializer, CategorySerializer
 from django.db.models import Count
 
 
 class ProductsList(ListCreateAPIView):
-    queryset = Product.objects.select_related("category").all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()  # --> Generic View te normal query kaj kore...
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     lookup_field = "id"
-
-    # # -----------> Logical bepar-separ thakle sadharonto use kori...
-    # def delete(self, request, id):
-    #     product = get_object_or_404(Product, pk=id)
-    #     if product.stock > 10:
-    #         return Response(
-    #             {"message": "Product with stock more than 10 could't be delete !"}
-    #         )
-    #     product.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CategoryList(ListCreateAPIView):
