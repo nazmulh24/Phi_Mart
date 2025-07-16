4eb55c85c72cb18519ded87c868669e98ca428a8
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-16 19:01:35 +0600

    Serializer Nesting for Cart Items

--- order/serializers.py
+++ order/serializers.py
@@ -1,9 +1,38 @@
 from rest_framework import serializers
 
-from order.models import Cart
+from order.models import Cart, CartItem
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "price"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+
+    total_price = serializers.SerializerMethodField(method_name="get_total_price")
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity", "product", "total_price"]
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.product.price * cart_item.quantity
 
 
 class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True)
+
+    Total_Price = serializers.SerializerMethodField(method_name="get_total_price")
+
     class Meta:
         model = Cart
-        fields = ["id", "user"]
+        fields = ["id", "user", "items", "Total_Price"]
+
+    def get_total_price(self, cart: Cart):
+        list = sum([item.product.price * item.quantity for item in cart.items.all()])
+        return list
