3c85a01bc26dbcc8b63b90dfc25a874727c51bdf
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-16 15:14:55 +0600

    Add Django_filter for more than 1 filter...

--- product/views.py
+++ product/views.py
@@ -2,21 +2,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from product.models import Product, Category, Review
 from product.serializers import ProductSerializer, CategorySerializer, ReviewSerializer
 from django.db.models import Count
 
 
 class ProductViewSet(ModelViewSet):
-    # queryset = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        category_id = self.request.query_params.get("category_id")
-        if category_id is not None:
-            queryset = Product.objects.filter(category_id=category_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["category_id", "price"]
+
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     category_id = self.request.query_params.get("category_id")
+    #     if category_id is not None:
+    #         queryset = Product.objects.filter(category_id=category_id)
+    #     return queryset
 
     # ---> Custom filter for delete an item...|--> delete not allow when--> product>10
     def destroy(self, request, *args, **kwargs):
