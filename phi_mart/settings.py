ef36a11219956649dffa3b0a80d03e2fba0856d7
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-18 15:30:16 +0600

    Login User Using JWT

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,6 +152,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 
