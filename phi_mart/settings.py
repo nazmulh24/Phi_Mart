ae9bc2915aa257c71df05bd3d36886fb6c71515d
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-18 14:53:38 +0600

    Setup Joser and Simple JWT

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     #
     "django_filters",
     "rest_framework",
+    "djoser",
     "debug_toolbar",
     #
     "api",
@@ -143,4 +144,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
 }
