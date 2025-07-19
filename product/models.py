31861d7de7a26c2d817a5a71688e5fd369b9e552
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-19 11:29:38 +0600

    Create environment for image field (MEDIA)

--- product/models.py
+++ product/models.py
@@ -16,7 +16,6 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to="products/images", blank=True, null=True)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="products"
     )
@@ -30,6 +29,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images",
+    )
+    image = models.ImageField(upload_to="products/images")
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
 
 
 class Review(models.Model):
